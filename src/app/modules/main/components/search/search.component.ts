0dd903c45e4c8a18aaf3460b7809ee9ce15f682c
Yurii <zumbala2013@gmail.com>
2024-02-21 05:36:25 +0200

    Configure Search logic

--- src/app/modules/main/components/search/search.component.ts
+++ src/app/modules/main/components/search/search.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { TripService } from '../../services/trip.service';
 
 @Component({
   selector: 'search',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './search.component.html',
-  styleUrl: './search.component.scss'
+  styleUrl: './search.component.scss',
 })
 export class SearchComponent {
+  inputValue: string = '';
 
+  constructor(private tripService: TripService) {}
+
+  onInput() {
+    if (this.inputValue) {
+      this.tripService.fetchTripsByInput(this.inputValue);
+    } else {
+      this.tripService.fetchTrips();
+    }
+  }
 }
--- src/app/modules/main/services/trip.service.ts
+++ src/app/modules/main/services/trip.service.ts
@@ -1,9 +1,68 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { Trip } from '../models/Trip';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TripService {
+  private tripsKey = 'trips';
+  private trips$ = new BehaviorSubject<Trip[]>([]);
+  readonly updatedTrips$ = this.trips$.asObservable();
 
-  constructor() { }
+  constructor() {
+    this.loadMock();
+  }
+
+  fetchTrips(): void {
+    this.trips$.next(this.storedTrips);
+  }
+
+  fetchTripsByInput(input: string): void {
+    const trips = this.storedTrips;
+
+    const filteredTrips = trips.filter((trip) => trip.city.toLocaleLowerCase().includes(input));
+
+    if (filteredTrips) {
+      this.trips$.next(filteredTrips);
+    }
+  }
+
+  addTrip(trip: Trip): void {
+    const trips = this.storedTrips;
+
+    trips.push(trip);
+
+    localStorage.setItem(this.tripsKey, JSON.stringify(trips));
+
+    this.fetchTrips();
+  }
+
+  getTripById(id: number): Trip {
+    const trips = this.storedTrips;
+
+    const tripInDB = trips.find((el) => el.id === id) as Trip;
+
+    return tripInDB;
+  }
+
+  private get storedTrips(): Trip[] {
+    return JSON.parse(localStorage.getItem(this.tripsKey) || '{}') as Trip[];
+  }
+
+  loadMock(): void {
+    if (
+      localStorage.getItem(this.tripsKey) === null ||
+      localStorage.getItem(this.tripsKey) == undefined
+    ) {
+      let trips = [
+        new Trip(0, 'Berlin', 1708479173, 1708825553),
+        new Trip(1, 'Lviv', 1708998353, 1709171153),
+      ];
+
+      localStorage.setItem(this.tripsKey, JSON.stringify(trips));
+      this.fetchTrips();
+      return;
+    }
+  }
 }
